557502649538410b29e6fab72202d991069d6b2b
suxiaoshao <2108412077@qq.com>
2020-10-07 21:04:34 +0800

    local icon and font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './style/index.scss';
+import 'fontsource-roboto';
 sessionStorage.setItem('theme', String(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches));
 ReactDOM.render(
   <React.StrictMode>
--- src/util/config.ts
+++ src/util/config.ts
@@ -1,4 +1,4 @@
-export const wallpaperNum = 6;
+export const wallpaperNum = 3;
 export const articleLimit = 10;
 export const baseUrl = 'https://www.sushao.top/api/visitor';
 export const replyLimit = 10;
